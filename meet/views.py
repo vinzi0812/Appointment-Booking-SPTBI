6f8c92e96263eaa700d83a8d58567f160623fe2d
Vineet Parmar <vineet.parmar@spit.ac.in>
2023-06-30 16:13:35 +0530

    final

--- meet/views.py
+++ meet/views.py
@@ -55,7 +55,7 @@ def index(request):
     timeslots = aTimeSlot.objects.filter(date = date)
     num = range(24)
     r = range(3)
-    return render(request, 'cal.html', {'timeslots':timeslots, 'num':num, 'r': r, 'date': date, 'times': times, 'status':status})
+    return render(request, 'index.html', {'timeslots':timeslots, 'num':num, 'r': r, 'date': date, 'times': times, 'status':status})
 
 def delete_slot(request):
     times = ["09.00 am", "09.30 am","10.00 am", "10.30 am", "11.00 am", "11.30 am", "12.00 pm", "12.30 pm", "01.00 pm", "01.30 pm", "02.00 pm", "02.30 pm","03.00 pm", "03.30 pm","04.00 pm", "04.30 pm","05.00 pm", "05.30 pm","06.00 pm", "06.30 pm","07.00 pm", "07.30 pm","08.00 pm", "08.30 pm"]
